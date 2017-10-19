22c217a2a962372fbf2b63f5bcc21e6623768f3a
Jean-Philippe Eisenbarth <jpeisenbarth@users.noreply.github.com>
2017-10-19 14:42:53 +0200

    [add] Copyright notice + license file and notices

--- config/config.go
+++ config/config.go
@@ -1,3 +1,20 @@
+// Copyright 2017 Jean-Philippe Eisenbarth
+//
+// This file is part of Mute Authentication Proxy.
+//
+// Foobar is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Mute Authentication Proxy is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with Foobar. See the file COPYING.  If not, see <http://www.gnu.org/licenses/>.
+
 package config
 
 import (
